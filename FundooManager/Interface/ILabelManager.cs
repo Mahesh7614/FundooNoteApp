bdc67c6d66a046465878a129f0d31e81e5490b6d
Mahesh7614 <waghmaremahesh012@gmail.com>
2023-01-02 15:55:51 +0530

    Add,Update,Delete,GetLabels

---
+++ FundooManager/Interface/ILabelManager.cs
@@ -0,0 +1,12 @@
+﻿using FundooModel;
+
+namespace FundooManager.Interface
+{
+    public interface ILabelManager
+    {
+        public bool AddLabel(string label, int noteID, int userID);
+        public List<LabelModel> GetLables(int noteID);
+        public bool UpdateLabel(string newlabel, int labelID);
+        public bool DeleteLabel(int labelID);
+    }
+}
---
+++ FundooModel/LabelModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FundooModel
+{
+    public class LabelModel
+    {
+        public int LabelID { get; set; }
+        public string LabelName { get; set; }
+        public int UserID { get; set; }
+        public int NoteID { get; set; }
+    }
+}
